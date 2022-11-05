fec9a68a993675ce19c31490d1242c73ba234892
Christian <christiansmith2394@gmail.com>
2022-11-05 18:20:54 -0400

    Major bug, won't load Dashboard, bar chart, pie chart, line chart has no lines

--- src/scenes/global/TopBar.jsx
+++ src/scenes/global/TopBar.jsx
@@ -8,15 +8,16 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
+import React from "react";
 
-function TopBar() {
+const Topbar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
 
   return (
     <Box display="flex" justifyContent="space-between" p={2}>
-      {/* Search Bar  */}
+      {/* SEARCH BAR */}
       <Box
         display="flex"
         backgroundColor={colors.primary[400]}
@@ -27,7 +28,8 @@ function TopBar() {
           <SearchIcon />
         </IconButton>
       </Box>
-      {/* Icons Section  */}
+
+      {/* ICONS */}
       <Box display="flex">
         <IconButton onClick={colorMode.toggleColorMode}>
           {theme.palette.mode === "dark" ? (
@@ -48,6 +50,6 @@ function TopBar() {
       </Box>
     </Box>
   );
-}
+};
 
-export default TopBar;
+export default Topbar;
